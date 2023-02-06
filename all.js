7ae5dad05ff27348a68655b589cd2800712a5eec
SCY <shen421226@gmail.com>
2023-02-06 17:54:04 +0800

    手機板(未完成)

--- all.js
+++ all.js
@@ -5,19 +5,50 @@ const rightBg = document.querySelector('.right-bg')
 const left = document.querySelector('.left')
 const banner = document.querySelector('.banner')
 const logo = document.querySelector('.logo')
+const leftBtn = document.querySelector('#leftBtn')
 
-let width = 0;
-let height = 0;
-let mouseX = 0;
-let mouseY = 0;
-let mouseLeaveDelay = null;
-
-leftImg.addEventListener('mousemove', handleMouseMove)
-leftImg.addEventListener('mouseenter', handleMouseEnter)
-leftImg.addEventListener('mouseleave', handleMouseLeave)
-rightImg.addEventListener('mousemove', handleMouseMove)
-rightImg.addEventListener('mouseenter', handleMouseEnter)
-rightImg.addEventListener('mouseleave', handleMouseLeave)
+if (leftImg) {
+  let mouseX = 0;
+  let mouseY = 0;
+  let mouseLeaveDelay = null;
+  
+  leftImg.addEventListener('mousemove', handleMouseMove)
+  leftImg.addEventListener('mouseenter', handleMouseEnter)
+  leftImg.addEventListener('mouseleave', handleMouseLeave)
+  rightImg.addEventListener('mousemove', handleMouseMove)
+  rightImg.addEventListener('mouseenter', handleMouseEnter)
+  rightImg.addEventListener('mouseleave', handleMouseLeave)
+  
+  function handleMouseMove(e) {
+    
+    mouseX = e.offsetX - e.target.offsetWidth / 2
+    mouseY = e.offsetY - e.target.offsetHeight / 2  
+    // console.log(e);
+    
+    let mousePX = mouseX / e.target.offsetWidth
+    let mousePY = mouseY / e.target.offsetHeight
+    
+    const rX = mousePX * 30
+    const rY = mousePY * -30
+    
+    const tX = mousePX * -50
+    const tY = mousePY * -50
+    
+    e.target.style.transform = `translateX(${tX}px) translateY(calc(-50% + ${tY}px)) rotateY(${rY}deg) rotateX(${rX}deg)`
+  }
+  
+  function handleMouseEnter() {
+    clearTimeout(mouseLeaveDelay)
+  }
+  
+  function handleMouseLeave(e) {
+    mouseLeaveDelay = setTimeout(() => {
+      e.target.style.transform = `translateX(0px) translateY(calc(-50%))`
+      mouseX = 0 
+      mouseY = 0
+    }, 500)
+  }
+}
 
 window.addEventListener('scroll', () => {
   if (window.scrollY > banner.offsetHeight -100) {
@@ -28,42 +59,3 @@ window.addEventListener('scroll', () => {
     logo.classList.add('logo-bg')
   }
 })
-
-
-function handleMouseMove(e) {
-  if(e.target === leftImg) {
-    mouseX = e.pageX - container.offsetLeft - e.target.offsetLeft - e.target.offsetWidth / 2
-    mouseY = e.pageY - 1105 - e.target.offsetHeight / 2
-  } else {
-    mouseX = e.pageX - e.target.offsetLeft - rightBg.offsetLeft - e.target.offsetWidth / 2
-    mouseY = e.pageY - 2249 - e.target.offsetHeight / 2
-  }
-  // console.log(e.pageY, mouseY);
-  
-  
-  let mousePX = mouseX / e.target.offsetWidth
-  let mousePY = mouseY / e.target.offsetHeight
-  
-  const rX = mousePX * 30
-  const rY = mousePY * -30
-
-  const tX = mousePX * -40
-  const tY = mousePY * -40
-
-  e.target.style.transform = `translateX(${tX}px) translateY(calc(-50% + ${tY}px)) rotateY(${rY}deg) rotateX(${rX}deg)`
-  
-  // console.log(e.target.style.translateX, e.target.style.transform);
-}
-
-
-function handleMouseEnter() {
-  clearTimeout(mouseLeaveDelay)
-}
-
-function handleMouseLeave(e) {
-  mouseLeaveDelay = setTimeout(() => {
-    e.target.style.transform = `translateX(0px) translateY(calc(-50%))`
-    mouseX = 0 
-    mouseY = 0
-  }, 500)
-}
